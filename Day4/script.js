c424b7d329339c8c6e565927c698b9048b9dd81a
Madhurchhetri <tandonnakul987@gmail.com>
2025-12-08 20:34:02 +0530

    Day4

---
+++ Day4/script.js
@@ -0,0 +1,34 @@
+// ............... Example of CallBack Function .............
+
+// setTimeout(() => {
+//     console.log('hii');
+    
+// }, 200);
+
+// getDataFromInstagram("ayush123",function(){
+
+// })
+
+// function abcd(fn){
+//     fn();
+// }
+
+// abcd(function(){
+//     console.log('hello');
+    
+// })
+
+// function iceCreamLao(address,fn){
+//     console.log("fetching....");
+
+//     setTimeout(()=>{
+//         fn("iceCream")
+//     },2000)
+    
+    
+// }
+
+// iceCreamLao("adndsnjs",function(ice){
+//     console.log(ice);
+    
+// })
