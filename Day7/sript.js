594de00e6a2988ac0287db45b355edfa5be77604
Madhurchhetri <tandonnakul987@gmail.com>
2025-12-10 21:52:14 +0530

    day 7

---
+++ Day7/sript.js
@@ -0,0 +1,38 @@
+// runtime error code likhte waqt error nahi ha jab code chalega uske baad error pata lagega
+
+
+// function abcd(){
+//     let a = 12;
+//     console.log(a.name);
+    
+// }
+// abcd();
+
+// ............................logical errors................
+
+// function add(a,b){
+//     return a -b ;
+// }
+// add(1,2)
+
+// ..................understanding the error object - [message, name ,stack].............
+// try{
+//     let a = 12;
+//     console.log(a.age.first);
+    
+// }catch(error){
+//     console.error(error.message);
+//      console.error(error.stack);
+//      console.error(error.name);
+// }
+// console.log('huihui');
+
+// ................how to throw error in javascript...............
+
+// try{
+//     let a = 12;
+//     console.log(a.name.age);
+    
+// }catch(error){
+//     throw new Error('This is custom error message');
+// }
